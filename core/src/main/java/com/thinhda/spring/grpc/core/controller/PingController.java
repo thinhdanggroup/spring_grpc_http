3f357d08e900ac11625b4be31b70884a50f9ddf4
thinhda <dinhluchvc@gmail.com>
2019-08-18 14:30:28 +0700

    edit api message

--- core/src/main/java/com/thinhda/spring/grpc/core/controller/PingController.java
+++ core/src/main/java/com/thinhda/spring/grpc/core/controller/PingController.java
@@ -1,24 +1,28 @@
 package com.thinhda.spring.grpc.core.controller;
 
+import com.thinhda.spring.grpc.core.model.PingRequest;
+import com.thinhda.spring.grpc.core.model.PingResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/api")
 @Slf4j
 public class PingController {
 
-    @GetMapping("/ping")
-    public ResponseEntity<?> ping() {
-        log.debug("Ping message from ", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        return ResponseEntity.ok("");
+    @PostMapping(path = "/ping", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> ping(@RequestBody PingRequest request) {
+        log.debug("Ping message from {}", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        return ResponseEntity.ok(PingResponse.newBuilder()
+            .setTimestamp(request.getTimestamp())
+            .setMessage("Pong")
+            .build());
     }
 
 }
