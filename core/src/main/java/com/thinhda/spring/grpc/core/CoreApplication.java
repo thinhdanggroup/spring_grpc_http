69da5bd8fc9630521f48887d230e630554ce085a
thinhda <dinhluchvc@gmail.com>
2019-08-18 14:39:17 +0700

    edit main file

--- core/src/main/java/com/thinhda/spring/grpc/core/CoreApplication.java
+++ core/src/main/java/com/thinhda/spring/grpc/core/CoreApplication.java
@@ -1,21 +1,34 @@
 package com.thinhda.spring.grpc.core;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+import java.io.File;
 
 /**
  * Created by thinhda.
  * Date: 2019-08-18
  */
 @SpringBootApplication
-public class CoreApplication {
+public class CoreApplication implements DisposableBean, CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        String pidFile = System.getProperty("pidfile");
+        if (pidFile != null) {
+            new File(pidFile).deleteOnExit();
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
 }
